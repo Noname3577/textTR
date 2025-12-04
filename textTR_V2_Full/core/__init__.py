fc4c41bd1d73c838f99fdcc986115b67c1413c5b
LV.1 <zone3577zone@gmail.com>
2025-12-04 05:23:40 +0000

    .

--- textTR_V2_Full/core/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Core functionality package
-"""
-
-from core.text_splitter import split_text_file, merge_text_files
-from core.translation_engine import TranslationEngine
--- textTR_V2_Full/data/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Data management package
-"""
-
-from data.translation_data import TranslationData
